cf83857ffb9654d48f6806ad295f67ce3a3e3359
olivers86 <44843263+olivers86@users.noreply.github.com>
2019-04-11 14:25:27 +0200

    Latest code, clock is still not running at correct frequency

---
+++ greenhouse_main_ready_v.1/arduino_secrets.h
@@ -0,0 +1,8 @@
+#define SECRET_SSID "olivers iPhone"
+#define SECRET_PASS "ntiv4wd4kkiax"
+
+#define SECRET_SSID "HUAWEI P20 Pro"
+#define SECRET_PASS "0709865400"
+
+#define CLOUD_USER "greenhouse"
+#define CLOUD_PASS "Qw0912"
