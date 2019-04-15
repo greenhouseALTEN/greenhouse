e244691e4ec2aae5765baaa4321f17ca03a6a6ac
olivers86 <44843263+olivers86@users.noreply.github.com>
2019-04-15 08:11:54 +0200

    RTC register updated to time more accurate

--- greenhouse_main_ready_v.1/arduino_secrets.h
+++ greenhouse_main_ready_v.1/arduino_secrets.h
@@ -1,8 +1,8 @@
-#define SECRET_SSID "olivers iPhone"
-#define SECRET_PASS "ntiv4wd4kkiax"
+#define SECRET_SSID "sverigessnabbaste"
+#define SECRET_PASS "lilleskutt86"
 
-#define SECRET_SSID "HUAWEI P20 Pro"
-#define SECRET_PASS "0709865400"
+//#define SECRET_SSID "olivers iPhone"
+//#define SECRET_PASS "ntiv4wd4kkiax"
 
 #define CLOUD_USER "greenhouse"
 #define CLOUD_PASS "Qw0912"
